c09976a1206b1af66ecb54ed5696fe8d5aa39496
rightfold <rightfold@gmail.com>
2017-02-08 15:08:20 +0100

    Add tests

---
+++ test/Main.purs
@@ -0,0 +1,32 @@
+module Test.Main
+  ( main
+  ) where
+
+import Data.ByteString as ByteString
+import Data.Maybe (Maybe(..))
+import Database.Redis as Redis
+import Node.Encoding (Encoding(UTF8))
+import Prelude
+import Test.Unit (test)
+import Test.Unit.Assert as Assert
+import Test.Unit.Main (runTest)
+
+main = runTest do
+  test "Database.Redis" do
+    Redis.withConnection "redis://localhost" \redis -> do
+      let key1 = b "purescript-redis:test:key1"
+      let key2 = b "purescript-redis:test:key2"
+
+      Redis.del redis [key1, key2]
+
+      do
+        let set = b "value1"
+        Redis.set redis key1 set
+        got <- Redis.get redis key1
+        Assert.equal (Just set) got
+
+      do
+        got <- Redis.incr redis key2
+        Assert.equal 1 got
+  where
+  b = ByteString.fromString `flip` UTF8
