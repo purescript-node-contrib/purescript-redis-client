86baadf1e607dcaef7c8f725af5eb1108130789a
Tomasz Rybarczyk <paluho@gmail.com>
2018-05-07 16:39:02 +0200

    Code cleanup

--- test/Main.purs
+++ test/Main.purs
@@ -4,38 +4,64 @@ module Test.Main
 
 import Prelude
 
-import Control.Monad.Aff (Milliseconds(Milliseconds), delay)
+import Control.Monad.Aff (Aff, Milliseconds(Milliseconds), delay)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.AVar (AVAR)
+import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Except (catchError, throwError)
 import Data.Array (sort)
 import Data.ByteString (ByteString)
 import Data.ByteString as ByteString
 import Data.Foldable (length)
 import Data.Maybe (Maybe(..))
-import Database.Redis (Expire(..), Write(..), flushdb, keys)
+import Database.Redis (Connection, Expire(..), REDIS, Write(..), flushdb, keys)
 import Database.Redis as Redis
-import Test.Unit (suite)
+import Test.Unit (TestSuite, suite)
 import Test.Unit as Test.Unit
 import Test.Unit.Assert as Assert
+import Test.Unit.Console (TESTOUTPUT)
 import Test.Unit.Main (runTest)
 
 b :: String -> ByteString
 b = ByteString.toUTF8
 
+test
+  :: forall t39 t40
+   . String
+  -> String
+  -> (Connection -> Aff ( redis :: REDIS | t39) t40)
+  -> TestSuite (redis :: REDIS | t39)
 test s title action =
   Test.Unit.test title $ do
     withFlushdb s action
 
+withFlushdb
+  :: ∀ a eff
+   . String
+  -> (Connection -> Aff ( redis :: REDIS | eff) a)
+  -> Aff ( redis :: REDIS | eff) Unit
 withFlushdb s action = Redis.withConnection s \conn -> do
   k <- keys conn (b "*")
   -- Safe guard
   Assert.assert  "Test database should be empty" (length k == 0)
   catchError (action conn >>= const (flushdb conn)) (\e -> flushdb conn >>= const (throwError e))
 
+main
+  :: forall eff
+  . Eff
+    ( console :: CONSOLE
+    , testOutput :: TESTOUTPUT
+    , avar :: AVAR
+    , redis :: REDIS
+    | eff
+    )
+    Unit
 main = runTest $ do
   let
     addr = "redis://127.0.0.1:43210"
     key1 = b "purescript-redis:test:key1"
     key2 = b "purescript-redis:test:key2"
+
   suite "Database.Redis" do
     test addr "set and get" $ \conn -> do
       let set = b "value1"
@@ -56,31 +82,31 @@ main = runTest $ do
     test addr "key expiration" $ \conn -> do
       let set = b "value1"
       Redis.set conn key1 set (Just (EX 1)) Nothing
-      got <- Redis.get conn key1
-      Assert.equal (Just set) got
+      got1 <- Redis.get conn key1
+      Assert.equal (Just set) got1
       delay (Milliseconds 1000.0)
-      got <- Redis.get conn key1
-      Assert.equal Nothing got
+      got2 <- Redis.get conn key1
+      Assert.equal Nothing got2
 
     test addr "set with XX" $ \conn -> do
       let set = b "value1"
       Redis.del conn [key1]
       Redis.set conn key1 set Nothing (Just XX)
-      got <- Redis.get conn key1
-      Assert.equal Nothing got
+      got1 <- Redis.get conn key1
+      Assert.equal Nothing got1
       Redis.set conn key1 set Nothing (Just NX)
-      got <- Redis.get conn key1
-      Assert.equal (Just set) got
+      got2 <- Redis.get conn key1
+      Assert.equal (Just set) got2
 
     test addr "set with NX" $ \conn -> do
      let set = b "value1"
      Redis.del conn [key1]
      Redis.set conn key1 set Nothing (Just NX)
-     got <- Redis.get conn key1
-     Assert.equal (Just set) got
+     got1 <- Redis.get conn key1
+     Assert.equal (Just set) got1
      Redis.set conn key1 (b "new") Nothing (Just NX)
-     got <- Redis.get conn key1
-     Assert.equal (Just set) got
+     got2 <- Redis.get conn key1
+     Assert.equal (Just set) got2
 
     test addr "mget" $ \conn -> do
       void $ Redis.incr conn key1
